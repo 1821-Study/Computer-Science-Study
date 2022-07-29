d354c8d993707a203866057ba3b21da26605512b
Dltmd202 <dltmd202@gmail.com>
2022-07-30 06:57:25 +0900

    Feat: Add Persistence Context study(#1)

---
+++ jpa/persistenceContext/Dltmd202/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            Member findMember1 = em.find(Member.class, 100L);
+            Member findMember2 = em.find(Member.class, 100L);
+
+            System.out.println(findMember1 == findMember2);
+
+            em.persist(member);
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
--- jpa/persistenceContext/README.md
+++
@@ -1,3 +0,0 @@
-# JPA Persistence API
-
-📅 
